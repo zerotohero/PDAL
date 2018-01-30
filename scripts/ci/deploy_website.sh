18ae51bb14738476de905d261910a9d65aed5271
Howard Butler <howard@hobu.co>
2018-01-30 15:45:14 -0600

    try adding everything for doc push

--- scripts/ci/deploy_website.sh
+++ scripts/ci/deploy_website.sh
@@ -19,7 +19,7 @@ cd $destdir/pdaldocs
 git config user.email "pdal@hobu.net"
 git config user.name "PDAL Travis docsbot"
 
-git add -A
+git add
 git commit -m "update with results of commit https://github.com/PDAL/PDAL/commit/$TRAVIS_COMMIT for ${DATE}"
 git push origin master
 
