fb15e2bb269d5a666f84aae8001eb07c3d3b4f83
Andrew Bell <andrew.bell.ia@gmail.com>
2018-12-03 14:32:53 -0500

    Deploy website.

--- scripts/ci/after_success.sh
+++ scripts/ci/after_success.sh
@@ -4,6 +4,13 @@ TRAVIS_BUILD_DIR=$1
 
 echo "publishing website"
 
-./build_docs.sh
-./add_deploy_key.sh
-./deploy_website.sh $TRAVIS_BUILD_DIR/doc/build /tmp
+if [ -n $encrypted_6a5172b96922_key ]
+then
+    ./build_docs.sh
+    # Args: keyfile, encrypted key, encrypted init vector
+    ./add_deploy_key.sh pdaldocs-private.key $encrypted_6a5172b96922_key \
+        $encrypted_6a5172b96922_iv
+    ./deploy_website.sh $TRAVIS_BUILD_DIR/doc/build /tmp
+else
+    echo "No encryption key available."
+fi
